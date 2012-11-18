bb82a675ad895efc88280b0eead9ffc6d6354deb
Dolf Schimmel (Freeaqingme) <dolfschimmel@gmail.com>
2012-11-18 23:57:14 +0100

    Made php::pear configurable

--- manifests/pear.pp
+++ manifests/pear.pp
@@ -5,13 +5,31 @@
 # Usage:
 # include php::pear
 #
-class php::pear  {
+# == Parameters
+#
+# Standard class parameters
+# Define the general class behaviour and customizations
+#
+# [*package*]
+#   Name of the package to install. Defaults to 'php-pear'
+#
+# [*install_package*]
+#   Boolean. Determines if any package should be installed to support the PEAR functionality.
+#   Can be false if PEAR was already provided by another package or module.
+#   Default: true
+#
+class php::pear (
+  $package = "php-pear",
+  $install_package = true
+  ) {
 
   include php
 
-  package { php-pear:
-    name   => 'php-pear',
-    ensure => present,
+  if ( $install_package ) {
+    package { php-pear:
+      name   => $package,
+      ensure => present,
+    }
   }
 
 }
