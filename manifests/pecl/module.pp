6c53259b5a269037793e0027bfa46bd1945284c7
erik-smit <smite@zylon.net>
2012-12-06 15:03:51 +0100

    Stray }

--- manifests/pecl/module.pp
+++ manifests/pecl/module.pp
@@ -48,7 +48,6 @@ define php::pecl::module (
             entry  => "PHP/extension[. = \"${name}.so\"]",
             value  => "${name}.so",
             notify => Service[$service],
-          }
         }
       }
     }
