e69fe95d014a428591e56ff82f4ba3c5140ca5e2
Dan Schaefer <dan@schaeferzone.net>
2013-07-17 09:32:53 -0400

    Adding a version parameter to php::pear

--- manifests/pear.pp
+++ manifests/pear.pp
@@ -13,6 +13,9 @@
 # [*package*]
 #   Name of the package to install. Defaults to 'php-pear'
 #
+# [*version*]
+#   Version to install. Defaults to 'present'
+#
 # [*install_package*]
 #   Boolean. Determines if any package should be installed to support the PEAR functionality.
 #   Can be false if PEAR was already provided by another package or module.
@@ -21,13 +24,14 @@
 class php::pear (
   $package         = $php::package_pear,
   $install_package = true,
+  $version         = 'present',
   $path            = '/usr/bin:/usr/sbin:/bin:/sbin'
   ) inherits php {
 
   if ( $install_package ) {
     package { 'php-pear':
       name   => $package,
-      ensure => present,
+      ensure => $version,
     }
   }
 
