286125be237afe2e6be374931e4c1cac86cca38d
Frederik Eychenié <frederik@almateos.com>
2014-08-26 16:36:40 +0200

    pear: fixes crash on invalid utf-8 sequence
    The `pear info` command may return some invalid utf-8 chars (like it does for `pear info drush/drush`).
    
    Piping the command output through `iconv -c` solves the thing.
    
    This solves #68

--- manifests/pear/module.pp
+++ manifests/pear/module.pp
@@ -59,13 +59,13 @@ define php::pear::module (
   }
 
   $pear_exec_unless = $ensure ? {
-    present => "pear info ${pear_source}",
+    present => "pear info ${pear_source} | iconv -c",
     absent  => undef
   }
 
   $pear_exec_onlyif = $ensure ? {
     present => undef,
-    absent  => "pear info ${pear_source}",
+    absent  => "pear info ${pear_source} | iconv -c",
   }
 
   $real_service = $service ? {
